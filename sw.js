646e99245ff2bee49087dc3e0064188867a3491a
Supermanslc <shawn.cohenmd@gmail.com>
2026-04-01 14:59:55 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,27 @@
-const CACHE_NAME = 'tolife-v6.3';
-const PRECACHE = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
-self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(PRECACHE)).then(() => self.skipWaiting())); });
-self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
+const CACHE = 'tolife-v6.5';
+const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
 self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
   const url = new URL(e.request.url);
-  if (url.pathname.endsWith('.html') || url.pathname === '/') {
-    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-  } else if (url.hostname.includes('api') || url.hostname.includes('gateway')) {
-    e.respondWith(fetch(e.request));
-  } else {
-    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).then(res => { if (res.ok) { const c = res.clone(); caches.open(CACHE_NAME).then(cache => cache.put(e.request, c)); } return res; })));
-  }
+  if (url.origin !== location.origin) return; // don't intercept external (API) calls
+  e.respondWith(
+    fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    }).catch(() => caches.match(e.request))
+  );
 });
