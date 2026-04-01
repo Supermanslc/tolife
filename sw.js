7cdb2443c6dfa3285253baa968ffe6230fa6b073
Supermanslc <shawn.cohenmd@gmail.com>
2026-04-01 15:41:59 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'tolife-v6.11';
+const CACHE = 'tolife-v7.0';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
